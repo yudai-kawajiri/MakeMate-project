f4aa73bd539003cc6043e9cc9982e171c8762149
Kawajiri Yudai <adgj2419@yahoo.co.jp>
2025-09-25 22:23:20 +0900

    feater/ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  # Chrome DevToolsのエラー抑制ルーティング (必要であれば)
+  get '/.well-known/appspecific/com.chrome.devtools.json', to: proc { [404, {}, ['']] }, constraints: { format: /json/ }
+
   devise_for :users
-  root to: 'homes#top' # ログイントップ画面
+
+  # ダッシュボードへのアクセスルート
+  get 'dashboard', to: 'dashboards#index'
+
+  # 認証状態によるルートの振り分け
+  devise_scope :user do
+    authenticated :user do
+      root to: 'dashboards#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
